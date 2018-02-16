1baca3a9de84eb208b21c9565d61b6611cddd6b0
Parvez391995 <rocksparvezmulla1@gmail.com>
2018-02-16 20:23:43 +0530

    first day

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventManagementSystem.Startup))]
+namespace EventManagementSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
